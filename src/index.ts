b0384b40dbc35693492bc743a68b39c2417e4f2d
davidpacco <david.j.pacco@gmail.com>
2024-09-19 10:59:28 -0500

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import diaryRouter from './routes/diaries';
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/diaries', diaryRouter);
 
